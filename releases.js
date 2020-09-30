46ad4dc35c6fa3ef91f8aa64b93f96e6dbc7b764
ManavKhorasiya <manavkhorasiya@gmail.com>
2020-09-30 21:20:56 +0530

    add functions

--- releases.js
+++ releases.js
@@ -1,5 +1,5 @@
 let { Octokit } = require("@octokit/core");
-let octokit = new Octokit({ auth : `INSERT TOKEN HERE FROM README`});
+let octokit = new Octokit({ auth : `INSERT KEY HERE FROM README`});
 let request = require('request');
 let fs = require('fs');
 let path = require('path');
@@ -26,15 +26,86 @@ let getLatestRelease = async function(repo) {
     let latestRelease = getLatestRelease(repo);
     latestRelease.then(response => {
         if(response.data.tag_name[0] == 'v') {
-            console.log(typeof response.data.tag_name)
-            response.data.tag_name.slice(1, response.data.tag_name.length)
-            console.log(response.data.tag_name);
+            response.data.tag_name = response.data.tag_name.slice(1, response.data.tag_name.length)
+            let latestVersion = response.data.tag_name;
+            if(latestVersion != version)
+            {
+                let latestAssetID = response.data.assets[0].id;
+                if(latestAssetID != assetsId)
+                {
+                    assetsId = latestAssetID;
+                }
+                version = latestVersion;
+            }
         }
-        console.log(response.data);
     });
 })();
 
+let downloadLatest = async function(assets_id) {
+    try {
+        return await octokit.request("GET /repos/{owner}/{repo}/releases/assets/{assets_id}", {
+            owner : 'ManavKhorasiya',
+            repo : 'Demo-Package',
+            assets_id : assets_id,
+        });
+    } catch (error) {
+        console.log(`Error fetching latest release binaries : ${error}`);
+    }
+};
 
+(async () => {
+    let releases = downloadLatest(assetsId);
+    releases.then(response => {
+        let url = response.data.url;
+        let name = response.data.name;
+        console.log(`Package name : ${name}`);
+        request(
+            {
+                url : url,
+                method : "GET",
+                encoding : null,
+                headers : {
+                    'Accept' : "application/octet-stream",
+                    Authorization : "INSERT KEY HERE FROM README",
+                    'User-Agent' : "request",
+                }
+            }, (err,response,body) => {
+                if(err) {
+                    console.log(err);
+                }
+                console.log(response.statusCode);
+                if(!err && response.statusCode == 200) {
+                    fs.writeFile('./temp/' + name, body, (err) => {
+                    if(err) {
+                        console.log(`Error occurred in writing file : ${err}`);
+                    } else {
+                        console.log('File written');
+                    }
+                    })
+                }
+            }
+        )
+    });
+})();
+
+const directoryPath = path.join(__dirname, 'temp');
+
+let extractAndReplace = (directoryPath) => {
+    fs.readdir(directoryPath, (err,files) => {
+        if(err) {
+            console.log(`Scan Directory Error : ${err}`);
+        }
+        console.log(files.length);
+        files.forEach((file) => {
+            console.log(`File is : ${file}`);
+            var zip = new AdmZip(path.join(directoryPath,file));
+            zip.extractAllTo(path.join(__dirname, "Downloaded Packages"),false);
+            console.log('extracted');
+        });
+    })
+}
+
+extractAndReplace(directoryPath);
 
 // octokit.request("GET /repos/{owner}/{repo}/releases/latest", {
 //     owner : 'ManavKhorasiya',
@@ -59,7 +130,7 @@ let getLatestRelease = async function(repo) {
 //       encoding : null,
 //       headers : {
 //         'Accept' : "application/octet-stream",
-//         Authorization : "INSERT TOKEN HERE FROM README",
+//         Authorization : "INSERT KEY HERE FROM README",
 //         'User-Agent' : "request",
 //       }
 //     }, (err,response,body) => {
